e83b9662c60a59d6e770367a1ec6810da39bce7b
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2024-01-22 13:43:34 -0600

    payment interface

--- src/components/payments/payments.int.tsx
+++ src/components/payments/payments.int.tsx
@@ -1,53 +1,144 @@
-import React, { useRef } from 'react';
-import printJS from 'print-js';
-import styles from './payment.int.module.css';
-import cashCircle from '../../assets/icon/cashCircle.svg';
-
+import printJS from "print-js";
+import styles from "./payment.int.module.css";
+// Icons
+import cashCircle from "../../assets/icon/cashCircle.svg";
+import closeIcon from "../../assets/icon/closeIcon.svg";
+import ActionsIcon from "../../assets/icon/actionsIcon.svg";
+import cashIcon from "../../assets/icon/cashIcon.svg";
+import cardIcon from "../../assets/icon/cardIcon.svg";
+import transferIcon from "../../assets/icon/transferIcon.svg";
+import checkLarge from "../../assets/icon/checkLarge.svg";
+import backLarge from "../../assets/icon/backLarge.svg";
+import blueDivider from "../../assets/icon/blueDivider.svg";
+import cashIconMedium from "../../assets/icon/cashSignalMedium.svg";
+// Hooks
+import useDate from "../../hooks/useDate";
+import { useRef } from "react";
+// Utils
+import { denominations, keyboard } from "./utils/denominations";
 interface Props {
-    openModal: any;
-    isOpen: any;
-    onClose: any;
-    children: any;
+  openModal: any;
+  isOpen: any;
+  onClose: any;
+  children: any;
 }
 
 export default function PaymentInterface({
-    openModal,
-    isOpen,
-    onClose,
-    children,
+  openModal,
+  isOpen,
+  onClose,
+  children,
 }: Props) {
-    const ticketRef = useRef(null);
+  // Date
+  const { currentDateTime, opcionesFecha }: any = useDate();
+  const formattedFecha = currentDateTime.toLocaleDateString(
+    "es-ES",
+    opcionesFecha
+  );
+  const ticketRef = useRef(null);
 
-    const handleImprimirTicket = () => {
-        printJS({
-            printable: ticketRef.current,
-            type: 'html',
-            targetStyles: ['*'],
-        });
-    };
+  const handleImprimirTicket = () => {
+    printJS({
+      printable: ticketRef.current,
+      type: "html",
+      targetStyles: ["*"],
+    });
+  };
 
-    return (
-        <div className={styles.screen}>
-            <section className={styles.modal}>
-                <div>
-                    <div>
-                        <img src={cashCircle} alt="cash-circle" />
-                        <h1>{children}</h1>
-                    </div>
-                    <button className={styles.closeButton} onClick={onClose}>
-                        X
-                    </button>
-                </div>
-                <div ref={ticketRef}>
-                    <h2>Tomate taqueria</h2>
-                    <p>Fecha: {new Date().toLocaleDateString()}</p>
-                </div>
-                <div>
-                    <button className={styles.ticket} onClick={handleImprimirTicket}>
-                        Imprimir Ticket
-                    </button>
-                </div>
-            </section>
+  return (
+    <div className={styles.screen}>
+      <section className={styles.modal}>
+        <div>
+          <div>
+            <img src={cashCircle} alt="cash-circle" />
+            <h1>{children}</h1>
+          </div>
+          <span>{formattedFecha}</span>
+          <button className={styles.closeButton} onClick={onClose}>
+            <img src={closeIcon} alt="close-icon" />
+          </button>
         </div>
-    );
+        <div ref={ticketRef}>
+          <div>
+            <h3>Cuenta 000</h3>
+            {/* Aca pondremos un select con las notas en caso de que alla notas
+            disponibles */}
+          </div>
+          <div>
+            <h3>Total: $00.00</h3>
+            <button className={styles.actionBtn}>
+              <img src={ActionsIcon} alt="burguer-menu" />
+            </button>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div className={styles.totalContainer}>
+              <h2>$0.00</h2>
+            </div>
+            <div>
+              <button className={styles.keyboardBtn}>
+                <img src={cashIcon} alt="cash-icon" />
+                Efectivo
+              </button>
+              <button className={styles.keyboardBtn}>
+                <img src={cardIcon} alt="card-icon" />
+                Débito
+              </button>
+              <button className={styles.keyboardBtn}>
+                <img src={cardIcon} alt="card-icon" />
+                Crédito
+              </button>
+              <button className={styles.keyboardBtn}>
+                <img src={transferIcon} alt="spei-icon" />
+                Transferencia
+              </button>
+            </div>
+            <div>
+              <div>
+                {keyboard?.map((item) => (
+                  <button className={styles.keyboardItems}>{item}</button>
+                ))}
+              </div>
+              <div className={styles.denominationsContainer}>
+                {denominations?.map((item) => (
+                  <button className={styles.denominationBtn}>${item}</button>
+                ))}
+                <button className={styles.denominationBtn}>
+                  <img src={checkLarge} alt="check-large-icon" />
+                </button>
+                <button className={styles.denominationBtn}>
+                  <img src={backLarge} alt="back-large-icon" />
+                </button>
+              </div>
+            </div>
+            {/*  <button className={styles.ticket} onClick={handleImprimirTicket}>
+              Imprimir Ticket
+            </button>    */}
+          </div>
+          <div>
+            <div>
+              <div>
+                <h4>Forma de pago</h4>
+                <h4>Importe</h4>
+              </div>
+
+              <img src={blueDivider} alt="divider-blue-icon" />
+              <div className={styles.addPayContainer}></div>
+              <img src={blueDivider} alt="divider-blue-icon" />
+              <div className={styles.payTotal}>
+                <h4>Diferencia</h4>
+                <h4>$0.00</h4>
+              </div>
+            </div>
+
+            <button className={styles.payBtn}>
+              <img src={cashIconMedium} alt="cash-icon-medium" />
+              Pagar
+            </button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
