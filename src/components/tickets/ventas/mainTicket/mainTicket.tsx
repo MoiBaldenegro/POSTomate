36e5f21bf3f53a399222905733d0a086dd0f335a
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2024-01-23 16:09:59 -0600

    add logo

--- src/components/tickets/ventas/mainTicket/mainTicket.tsx
+++ src/components/tickets/ventas/mainTicket/mainTicket.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./mainTicket.module.css";
+import tomate from "../../../../assets/icon/tomatePOSlogo.svg";
 
 const TicketComponent = React.forwardRef((props, ref) => {
   return (
     <div className={styles.container}>
       <h2>Ticket de Compra</h2>
+      <img src={tomate} alt="tomate" />
       <p>Fecha: {new Date().toLocaleDateString()}</p>
       <p>Producto 1 - $10.00</p>
       <p>Producto 2 - $20.00</p>
