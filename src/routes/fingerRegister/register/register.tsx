1bc519fb2a00c3035722238162b581d94a6cc22e
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2024-02-21 14:22:19 -0600

    styles session febrero 21

--- src/routes/fingerRegister/register/register.tsx
+++ src/routes/fingerRegister/register/register.tsx
@@ -10,6 +10,7 @@ interface Props {
   user: User;
 }
 export default function Register({ user }: Props) {
+  const [startCapture, setStartCapture] = useState(false);
   const [active, setActive] = useState(false);
   return (
     <main className={styles.container}>
@@ -22,40 +23,51 @@ export default function Register({ user }: Props) {
           <div className={styles.handsContainer}>
             <div>
               <img className={styles.hand} src={rightHand} alt="right-hand" />
-              <div className={styles.fingerIndicator}></div>
+              {startCapture && <div className={styles.fingerIndicator}></div>}
             </div>
           </div>
           <div className={styles.indicatorsContainer}>
-            <p>
-              ¡Bienvenido! Para mejorar la seguridad, te invitamos a registrar
-              tu huella digital.
-            </p>
-            <div className={styles.instructions}>
-              <span>
-                - Asegúrate de que tus manos estén limpias y secas antes de
-                comenzar.
-              </span>{" "}
-              <br />
-              <span>
-                - Coloca tu dedo en el sensor y Presiona tu dedo con firmeza.
-              </span>
-              <br />
-              <span>
-                - Mantén tu dedo en posición hasta que veas una confirmación en
-                la pantalla.
-              </span>
-            </div>
-            <h2>Paso: {"1"}</h2>
-            <div className={styles.indicatorsItemContainer}>
-              <span className={styles.processIndicator}>1</span>
-              <span className={styles.processIndicator}>2</span>
-              <span className={styles.processIndicator}>3</span>
-              <span className={styles.processIndicator}>4</span>
-            </div>
-            <button className={styles.cancelBtn}>
-              <img src={crossCancel} alt="cross-cancel" />
-              Cancelar
-            </button>
+            {!startCapture && (
+              <div>
+                <p>
+                  ¡Bienvenido! Para mejorar la seguridad, te invitamos a
+                  registrar tu huella digital.
+                </p>
+                <span>
+                  Asegúrate de que tus manos estén limpias y secas antes de
+                  comenzar.
+                </span>
+                <button
+                  onClick={() => {
+                    setStartCapture(true);
+                  }}
+                  className={styles.startCaptureBtn}
+                >
+                  Iniciar captura
+                </button>
+              </div>
+            )}
+
+            {startCapture && (
+              <div>
+                <h2>Paso: {"1"}</h2>
+                <div className={styles.indicatorsItemContainer}>
+                  <span className={styles.processIndicator}>1</span>
+                  <span className={styles.processIndicator}>2</span>
+                  <span className={styles.processIndicator}>3</span>
+                  <span className={styles.processIndicator}>4</span>
+                </div>
+                <button
+                  className={styles.cancelBtn}
+                  onClick={() => {
+                    setStartCapture(false);
+                  }}
+                >
+                  <img src={crossCancel} alt="cross-cancel" />
+                  Cancelar
+                </button>
+              </div>
+            )}
           </div>
         </section>
       ) : (
